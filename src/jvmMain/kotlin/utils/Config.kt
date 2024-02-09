8139f5770e25fecb33164cda4c0ced4a9e9956bd
kdroidFilter <myriam.hadass@gmail.com>
2024-02-09 11:36:23 +0200

    improve config

--- src/jvmMain/kotlin/utils/Config.kt
+++ src/jvmMain/kotlin/utils/Config.kt
@@ -6,7 +6,7 @@ object Config {
     const val APP_ID = "com.kdroid.compose.desktop"
     const val ENABLE_TRAY_ICON = true
     const val ALLOW_ONLY_ONE_INSTANCE = true
-    const val PORT = 50152
+    const val PORT = 50152 //FOR IPC
 
     val AVAILABLE_LANGUAGES = listOf(
         Language("en", "English"),
